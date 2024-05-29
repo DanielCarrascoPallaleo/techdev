5057bae78eed3c6799607d1c495915544be3082f
JeysonFernandezF <82329309+JeysonFernandezF@users.noreply.github.com>
2024-05-29 19:42:43 -0400

    ADD: Validaciones

--- resources/views/components/validation-errors.blade.php
+++ resources/views/components/validation-errors.blade.php
@@ -1,10 +1,10 @@
 @if ($errors->any())
     <div {{ $attributes }}>
-        <div class="font-medium text-red-600 dark:text-red-400">{{ __('Whoops! Something went wrong.') }}</div>
+        <div class="font-medium text-red-600 dark:text-red-400">¡Ups! Algo ha salido mal.</div>
 
         <ul class="mt-3 list-disc list-inside text-sm text-red-600 dark:text-red-400">
             @foreach ($errors->all() as $error)
-                <li>{{ $error }}</li>
+                <li>Tus credenciales no coinciden con nuestros registros.</li>
             @endforeach
         </ul>
     </div>
